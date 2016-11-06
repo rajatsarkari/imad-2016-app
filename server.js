daeab043dc02aaeffdd430d5c1acf81482cce591
Rajat Sarkari <rajatsarkari777@gmail.com>
2016-11-06 18:39:14 +0530

    contact
    contact

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ app.use('/fonts', express.static(path.join(__dirname, 'ui', 'fonts')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+
 
 
 
