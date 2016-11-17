be4233a53da3105a88abcf8812a7e3e4bed5dacd
Rajat Sarkari <rajatsarkari777@gmail.com>
2016-11-17 18:23:29 +0530

    Modals for login window added
    Modals added

--- ui/main.js
+++ ui/main.js
@@ -15,9 +15,15 @@ $(document).ready(function () {
   });
 
 	createSkillProgress('.htmlbar', 0.6);
-	createSkillProgress('.cssbar', 0.7);
-
+	createSkillProgress('.cssbar', 0.65);
+	createSkillProgress('.javabar', 0.7);
+	createSkillProgress('.dbmsbar', 0.5);
+	createSkillProgress('.assemblybar', 0.7);
+	createSkillProgress('.cbar', 0.9);
+	createSkillProgress('.mbar', 0.8);
+  loadBlogPosts();
 });
+
 
 function createSkillProgress(skill, value) {
 	var bar = new ProgressBar.Circle(skill, {
@@ -29,5 +35,31 @@ function createSkillProgress(skill, value) {
 		}
 	});
 
-	bar.animate(value);
+	bar.animate(value, {duration: 1000});
+
+}
+
+function loadBlogPosts(){
+	$.ajax({
+		url: 'http://rsblog.getsandbox.com/posts',
+		method: 'GET',
+		success: function(res){
+			renderPosts(res.data);
+		},
+		error: function(err){
+			console.log(err);
+		}
+	});
+}
+
+function renderPosts(posts){
+	$('.post-container').find('.post').remove();
+	$.each(posts, function(i, post){
+		var wrapper = $('<div class="post"></div>');
+		var title = $('<h3 class="post-title">'+post.title+'</h3>');
+		var date = $('<h5 class="post-date">'+post.date+'</h5>');
+		var content = $('<p class="post-content">'+post.content+'</p>');
+		wrapper.append(title).append(date).append(content);
+		$('.post-container').append(wrapper);
+	});
 }
